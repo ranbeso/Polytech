09a85dabfd149ee7a0555b46451c115ca0420ab3
novo_sophia <novosophia14590@gmail.com>
2021-12-13 01:31:10 +0300

    update

--- homework2/main2.cpp
+++ homework2/main2.cpp
@@ -13,14 +13,16 @@ double height;
 double speed_x, speed_y;
 int check1 = 1, check2 = 0;
 
-void ReadFile(const char* path)
+void ReadFile(char* path)
 {
-    double x, y;
+    double x=0, y;
     std::ifstream in(path);
     if (in.is_open())
     {
         in >> height >> speed_x >> speed_y;
-        while (!in.eof() && speed_x >= x * (speed_y + sqrt(speed_y * speed_y + 2 * G * height)) / G)
+        point_X.push_back(0);
+        point_Y.push_back(height);
+        while (!in.eof() && check1 <= speed_x * (speed_y + sqrt(speed_y * speed_y + 2 * G * height)) / G)
         {
             in >> x >> y;
 
@@ -32,15 +34,19 @@ void ReadFile(const char* path)
 
 void FlyPoint(char ch) {
     if (ch == 'R')
-        height = -G * (point_X[check2 + 1] - point_X[check1 - 1]) * (point_X[check2 + 1] - point_X[check1 - 1]) / 2 / speed_x / speed_x + (speed_y - G * point_X[check1 + 1] / speed_x) * (point_X[check2 + 1] - point_X[check1 - 1]) / speed_x + height;
+        height = -G * (point_X[check2 + 1] - point_X[check1 - 1]) * (point_X[check2 + 1] - point_X[check1 - 1]) / 2 / speed_x / speed_x + (speed_y - G * point_X[check1 - 1] / speed_x) * (point_X[check2 + 1] - point_X[check1 - 1]) / speed_x + height;
     else
         height = -G * (point_X[check1] - point_X[check2 + 1]) * (point_X[check1] - point_X[check2 + 1]) / 2 / speed_x / speed_x - (speed_y - G * point_X[check2 + 1] / speed_x) * (point_X[check1] - point_X[check2 + 1]) / speed_x + height;
 
 }
 
+
+
+
+
 void Check(char ch) {
     if (ch == 'R') {
-        for (int i = check1; i <= point_X.size(); ++i) {
+        for (int i = check1; i <= point_X.size()-1; ++i) {
             double length = -G * (point_X[i] - point_X[check1 - 1]) * (point_X[i] - point_X[check1 - 1]) / 2 / speed_x / speed_x + (speed_y - G * point_X[check1 - 1] / speed_x) * (point_X[i] - point_X[check1 - 1]) / speed_x + height;
             if (length < point_Y[i]) {
                 check2 = i - 1;
@@ -51,19 +57,18 @@ void Check(char ch) {
     }
     else
     {
-        int n;
-        for (int i = 0; i <= check1; ++i) {
-            double length = -G * (point_X[check1 - i] - point_X[check1 + 1]) * (point_X[check1 - i] - point_X[check1 + 1]) / 2 / speed_x / speed_x - (speed_y - G * point_X[check1 + 1] / speed_x) * (point_X[check1 - i] - point_X[check1 + 1]) / speed_x + height;
+        for (int i = 0; i <= check2; ++i) {
+            double length = -G * (point_X[check2 - i] - point_X[check2 + 1]) * (point_X[check2 - i] - point_X[check2 + 1]) / 2 / speed_x / speed_x - (speed_y - G * point_X[check2 + 1] / speed_x) * (point_X[check2 - i] - point_X[check2 + 1]) / speed_x + height;
 
-            if (length < point_Y[check1 - i]) {
-                check1 -= i;
+            if (length < point_Y[check2 - i]) {
+                check1 = check2 - i;
                 return;
             }
-            else if (check1 - i == 0) {
-                check1 = 0;
+            else if (check2 - i == 0) {
+                check1 = check2 - i;
                 return;
             }
-            check1 = check1 - i;
+            check1 = check2 - i;
         }
     }
 }
@@ -71,19 +76,19 @@ void Check(char ch) {
 void Start() {
     while (height >= 0) {
         if (check1 == check2) {
-            cout << check1 << endl;
+            cout << check2 << endl;
             return;
         }
 
         Check('R');
-        if (check2 == point_X.size()) {
+        if (check2 == point_X.size()-1) {
             cout << check2 << endl;
             return;
         }
 
         FlyPoint('R');
         if (height < 0) {
-            cout << check2 << endl;
+            cout << int(check2) << endl;
             return;
         }
 
