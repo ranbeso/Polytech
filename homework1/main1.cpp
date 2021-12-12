af8208e6aa334d25bf95bfc4e40d0c4edbc8c7b9
novo_sophia <novosophia14590@gmail.com>
2021-12-13 01:41:34 +0300

    update

--- homework1/main1.cpp
+++ homework1/main1.cpp
@@ -1,109 +1,130 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
 #include <cmath>
-#include <algorithm>
-
-
-
-
+#include <climits>
+#include <vector>
 using namespace std;
 
-double Distance(double x, double y, double X, double Y) {
-    double d = fabs((y * Y - x * X) / sqrt(x * x + y * y));
+const double G = 9.81;
+
+vector<double> point_X;
+vector<double> point_Y;
+double height;
+double speed_x, speed_y;
+int check1 = 1, check2 = 0;
+
+void ReadFile(char* path)
+{
+    double x=0, y;
+    std::ifstream in(path);
+    if (in.is_open())
+    {
+        in >> height >> speed_x >> speed_y;
+        point_X.push_back(0);
+        point_Y.push_back(height);
+        while (!in.eof() && check1 <= speed_x * (speed_y + sqrt(speed_y * speed_y + 2 * G * height)) / G)
+        {
 
 
-    return d;
+            in >> x >> y;
+            if (x == int("") || y == int("")) return;
 
-}
-
-void ReadAll(const string& path) {
-    double XR_MAX = 0, XL_MAX = 0, YR_MAX = 0, YL_MAX = 0;
-    double DISTL = 0, DISTR = 0;
-    double d = 0;
-    double x = 0, y = 0, xV, yV, xr0 = 0, xl0 = 0, yr0 = 0, yl0 = 0;
-    bool ch = false;
-    ifstream file(path);
-    if (file.is_open()) {
-        string str;
-        int n = 0;
-        while (!file.eof()) {
-
-            //read vector
-            if (n == 0) {
-                file >> str;
-                xV = stoi(str);
-                file >> str;
-                yV = stoi(str);
-                n++;
-            }
-            str = "";
-            file >> str;
-            if (str == "") break;
-            //read x or y
-            x = stoi(str);
-            file >> str;
-            y = stoi(str);
-
-            if (xV > 0) {
-
-                if ((yV * x - xV * y < 0)) {
-                    d = fabs((yV * x - xV * y) / sqrt(xV * xV + yV * yV));
-                    if (d >= DISTL)
-                    {
-                        DISTL = d;
-                        XL_MAX = x;
-                        YL_MAX = y;
-                    }
-                }
-                else {
-                    d = fabs((yV * x - xV * y) / sqrt(xV * xV + yV * yV));
-                    xr0 = x;
-                    yr0 = y;
-                    if (d >= DISTR)
-                    {
-                        DISTR = d;
-                        XR_MAX = x;
-                        YR_MAX = y;
-                    }
-
-                }
-
-
-            }
-            else {
-                if (yV * x - xV * y < 0) {
-                    d = fabs((yV * x - xV * y) / sqrt(xV * xV + yV * yV));
-                    if (d >= DISTL)
-                    {
-                        DISTL = d;
-                        XL_MAX = x;
-                        YL_MAX = y;
-                    }
-                }
-                else {
-                    d = fabs((yV * x - xV * y) / sqrt(xV * xV + yV * yV));
-                    if (d >= DISTR)
-                    {
-                        DISTR = d;
-                        XR_MAX = x;
-                        YR_MAX = y;
-                    }
-                }
-            }
+            point_X.push_back(x);
+            point_Y.push_back(y);
         }
     }
-    file.close();
+}
 
-    cout << "Leftmost:" << ' ' << XL_MAX << ' ' << YL_MAX << endl;
-    cout << "Rightmost:" << ' ' << XR_MAX << ' ' << YR_MAX << endl;
+void FlyPoint(char ch) {
+    if (ch == 'R')
+        height = -G * (point_X[check2 + 1] - point_X[check1 - 1]) * (point_X[check2 + 1] - point_X[check1 - 1]) / 2 / speed_x / speed_x + (speed_y - G * point_X[check1 - 1] / speed_x) * (point_X[check2 + 1] - point_X[check1 - 1]) / speed_x + height;
+    else
+        height = -G * (point_X[check1] - point_X[check2 + 1]) * (point_X[check1] - point_X[check2 + 1]) / 2 / speed_x / speed_x - (speed_y - G * point_X[check2 + 1] / speed_x) * (point_X[check1] - point_X[check2 + 1]) / speed_x + height;
+
 }
 
 
-int main() {
-    const string path = "in.txt";
 
-    ReadAll("in.txt");
-    return 0;
+
+
+void Check(char ch) {
+    if (ch == 'R') {
+        for (int i = check1; i <= point_X.size()-1; ++i) {
+            double length = -G * (point_X[i] - point_X[check1 - 1]) * (point_X[i] - point_X[check1 - 1]) / 2 / speed_x / speed_x + (speed_y - G * point_X[check1 - 1] / speed_x) * (point_X[i] - point_X[check1 - 1]) / speed_x + height;
+            if (length < point_Y[i]) {
+                check2 = i - 1;
+                return;
+            }
+            check2 = i;
+        }
+    }
+    else
+    {
+        for (int i = 0; i <= check2; ++i) {
+            double length = -G * (point_X[check2 - i] - point_X[check2 + 1]) * (point_X[check2 - i] - point_X[check2 + 1]) / 2 / speed_x / speed_x - (speed_y - G * point_X[check2 + 1] / speed_x) * (point_X[check2 - i] - point_X[check2 + 1]) / speed_x + height;
+
+            if (length < point_Y[check2 - i]) {
+                check1 = check2 - i;
+                return;
+            }
+            else if (check2 - i == 0) {
+                check1 = check2 - i;
+                return;
+            }
+            check1 = check2 - i;
+        }
+    }
+}
+
+void Start() {
+    while (height >= 0) {
+        if (check1 == check2) {
+            cout << check2 << endl;
+            return;
+        }
+
+        Check('R');
+        if (check2 == point_X.size()-1) {
+            cout << check2 << endl;
+            return;
+        }
+
+        FlyPoint('R');
+        if (height < 0) {
+            cout << int(check2) << endl;
+            return;
+        }
+
+        Check('L');
+        if (check1 == 0) {
+            cout << check1 << endl;
+            return;
+        }
+
+        FlyPoint('L');
+        if (height < 0) {
+            cout << check1 << endl;
+            return;
+        }
+
+
+    }
+}
+
+
+
+int main(int argc, char** argv)
+{
+    if (argc == 2) {
+
+        ReadFile(argv[1]);
+
+        if (speed_x == 0 || point_X.size() == 0) {
+            cout << 0;
+            return 0;
+        }
+        Start();
+        return 0;
+    }
+    else return 0;
 }
